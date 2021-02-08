701b4b1845474d044fb29a2b17a60a06a36a3193
Evgeny Samsonov <bitniks@yandex.ru>
2021-02-08 21:12:21 +0300

    Reddit app terraform configs

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,24 +3,45 @@ terraform {
 }
 
 provider "yandex" {
-  version = "~> 0.35.0"
-  service_account_key_file = "/Users/evgeny/yc/iam/key.json"
-  zone = "ru-central1-a"
+  version                  = "~> 0.35.0"
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
+  zone = var.app_zone
   resources {
-    cores  = 1
+    cores  = 2
     memory = 2
   }
   boot_disk {
     initialize_params {
-      image_id = "fd81n997u4jihug42j14"
+      image_id = var.image_id
     }
   }
   network_interface {
-    subnet_id = "e9bu7e6cg1bggn4rqlh9"
+    subnet_id = var.subnet_id
     nat       = true
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    host        = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
+    private_key = file(var.app_connection_private_key)
+  }
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
   }
 }
