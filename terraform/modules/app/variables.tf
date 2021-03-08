3859d1ab29b5147097d41291c88cf16896bff5d0
Evgeny Samsonov <bitniks@yandex.ru>
2021-03-08 16:35:18 +0300

    Terraform modules

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,10 @@
+variable app_disk_image {
+  description = "Disk image for Reddit App"
+  default     = "reddit-app-base"
+}
+variable subnet_id {
+  description = "Subnet"
+}
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
